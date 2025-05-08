e1e12f21de13a156cd457a7add43a180cc82e9b8
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2025-05-08 11:21:10 +0200

    refactor: code cleanup

--- components/user/settings/components/baby-report.tsx
+++ components/user/settings/components/baby-report.tsx
@@ -11,13 +11,6 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
 import { Textarea } from "@/components/ui/textarea";
 import {
   BabyReportFormSchema,
