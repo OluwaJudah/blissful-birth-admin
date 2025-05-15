7d85cb3a017b6de1f5095b34605c018dfe7308d0
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2025-05-15 13:53:42 +0200

    feat: added login page to the landing page

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,17 @@
+import { Card } from "@/components/ui/card";
+import { UserAuthForm } from "@/components/login/components/user-auth-form";
+
+export default function SignIn() {
+  return (
+    <Card className="p-6">
+      <div className="flex flex-col space-y-2 text-left">
+        <h1 className="text-2xl font-semibold tracking-tight">Login</h1>
+        <p className="text-sm text-muted-foreground">
+          Enter your email and password below <br />
+          to log into your account
+        </p>
+      </div>
+      <UserAuthForm />
+    </Card>
+  );
+}
