114671ffd232af4652002e9e05e3ae73bf82349c
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2025-06-23 23:10:35 +0200

    refactor: updated sorting appointment by weeks

--- data/appointment.ts
+++ data/appointment.ts
@@ -56,7 +56,7 @@ export const getMotherAppointments = async (userId: string, fields = "") => {
     { userId: new Types.ObjectId(userId) },
     { __v: 0, createdAt: 0, updatedAt: 0 }
   )
-    .sort({ date: 1 })
+    .sort({ weeks: 1 })
     .lean();
 
   return appointments;
