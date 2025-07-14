c8821a1dcbbbeee2ff4839396e4a907cd5bc914f
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2025-07-14 11:46:54 +0200

    refactor: updated Reschedule text to Edit

--- components/user/settings/appointments/[appointmentId]/appointment-button.tsx
+++ components/user/settings/appointments/[appointmentId]/appointment-button.tsx
@@ -9,7 +9,7 @@ export function RescheduleAppointmmentButton() {
   return (
     <div className="flex gap-2">
       <Button className="space-x-1" onClick={() => setOpen("add")}>
-        <span>Reschedule</span> <IconEdit size={18} />
+        <span>Edit</span> <IconEdit size={18} />
       </Button>
     </div>
   );
