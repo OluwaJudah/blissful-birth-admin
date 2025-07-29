381db1dd899e88e99556d989d0abdc018fc4cdf9
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2025-07-29 23:13:04 +0200

    refactor: moving the pdf ccss file

--- app/api/print-appointments/[id]/route.ts
+++ app/api/print-appointments/[id]/route.ts
@@ -3,6 +3,7 @@ import puppeteer from "puppeteer-core";
 import { NextRequest } from "next/server";
 import { getMotherAppointments } from "@/data/appointment";
 import fs from "fs";
+import { generateAppointmentHTML } from "@/lib/generateHTML";
 
 export async function GET(
   req: NextRequest,
@@ -35,44 +36,8 @@ export async function GET(
             </tr>`
       );
     }, "");
-
+    const html = generateAppointmentHTML(base64Image, appointmentStr);
     // Generate your HTML here
-    const html = `
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <style>${css}</style>
-      </head>
-      <body class="p-10 text-sm">
-          <img src="${base64Image}" alt="Logo" class="w-1/2 mx-auto mb-3" />
-          <h2 class="text-xl font-bold text-center mb-4 underline">Appointment Schedule</h2>
-          <p>Co Registration: K2017378199</p>
-          <p>Practice Number: 0834106</p>
-          <p>SANC No: 11515228</p>
-          <p>Cell No: + 27 064 169 4887</p>
-          <p>Clinic Address: Medical Suites, Florida Junction</p>
-          <p>Birth Unit Address: 38 Simmer Street, Selwyn, Roodepoort</p>
-          <p class="mb-4">Generated on: ${new Date().toLocaleDateString()}</p>
-
-          <div class="flex justify-between w-3/4 pr-4">
-            <p>Name:</p>
-            <p>Tel No: </p>
-            <p>EDD: </p>
-          </div>  
-          <table class="w-full border-collapse border mx-4 text-xs">
-            <thead>
-              <tr class="bg-gray-200">
-                <th class="border p-2">Week</th>
-                <th class="border p-2">Date</th>
-                <th class="border p-2">Time</th>
-                <th class="border p-2">Notes</th>
-              </tr>
-            </thead>
-            <tbody>${appointmentStr}</tbody>
-          </table>
-        </body>    
-      </html>
-  `;
 
     const browser = await puppeteer.connect({
       browserWSEndpoint: `wss://production-sfo.browserless.io?token=${process.env.BROWSERLESS_TOKEN}`,
