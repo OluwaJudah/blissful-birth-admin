0f25142a6b67c81ab7635c36443b4c98eb289fc3
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2025-08-25 10:11:42 +0200

    refactor: screen orientation

--- components/orientation-lock.tsx
+++ components/orientation-lock.tsx
@@ -7,8 +7,8 @@ export default function OrientationLock() {
   useEffect(() => {
     if (typeof window !== "undefined" && "screen" in window) {
       const orientation: any = (window as any).screen?.orientation;
-      if (orientation && typeof orientation.lock === "function") {
-        orientation.lock("landscape").catch(() => {});
+      if (orientation && typeof orientation.unlock === "function") {
+        orientation.unlock().catch(() => {});
       }
     }
   }, []);
