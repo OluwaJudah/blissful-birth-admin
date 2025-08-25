d82cb0e4027f9c41d35177eca74c2c2e76a19b52
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2025-08-25 11:24:36 +0200

    feat: added checkbox

--- components/appointments/components/appointment-details.tsx
+++ components/appointments/components/appointment-details.tsx
@@ -2,7 +2,6 @@ import { Badge } from "@/components/ui/badge";
 import { IconChevronRight } from "@tabler/icons-react";
 import { User } from "lucide-react";
 import Link from "next/link";
-import React from "react";
 
 const AppointmentDetails = ({
   _id,
@@ -37,41 +36,45 @@ const AppointmentDetails = ({
   });
 
   return (
-    <Link
-      href={`/user/settings/${userId}/appointments/${_id}`}
-      className="rounded-3xl border flex justify-between items-center gap-3 px-2 py-1 hover:shadow-md"
-    >
-      <div className="flex items-center gap-3">
-        {" "}
-        <div className="flex items-center justify-between">
-          <div
-            className={`flex w-9 h-9 items-center justify-center rounded-full bg-muted p-2`}
-          >
-            <User />
-          </div>
-        </div>
-        <div className="">
-          <h2 className="text-sm font-semibold leading-none">
-            {fullName} {surname}
-          </h2>
-          <div className="flex flex-row gap-3 text-xs">
-            <div>
-              Weeks: <b>{pregnancyWeeks}</b>
-            </div>
-            <div>
-              Date: <b>{formattedDate}</b>
-            </div>{" "}
-            <div>
-              EDD: <b>{eddFormattedDate}</b>
+    <div className="flex flex-row gap-2 w-full items-center">
+      <input type="checkbox" className="w-5 h-5" />
+
+      <Link
+        href={`/user/settings/${userId}/appointments/${_id}`}
+        className="rounded-3xl border flex justify-between items-center gap-3 px-2 py-1 hover:shadow-md w-full"
+      >
+        <div className="flex items-center gap-3">
+          {" "}
+          <div className="flex items-center justify-between">
+            <div
+              className={`flex w-9 h-9 items-center justify-center rounded-full bg-muted p-2`}
+            >
+              <User />
             </div>
           </div>
+          <div className="">
+            <h2 className="text-sm font-semibold leading-none">
+              {fullName} {surname}
+            </h2>
+            <div className="flex flex-row gap-3 text-xs">
+              <div>
+                Weeks: <b>{pregnancyWeeks}</b>
+              </div>
+              <div>
+                Date: <b>{formattedDate}</b>
+              </div>{" "}
+              <div>
+                EDD: <b>{eddFormattedDate}</b>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="flex gap-3">
-        <Badge variant="outline">{status}</Badge>
-        <IconChevronRight />
-      </div>
-    </Link>
+        <div className="flex gap-3">
+          <Badge variant="outline">{status}</Badge>
+          <IconChevronRight />
+        </div>
+      </Link>
+    </div>
   );
 };
 
