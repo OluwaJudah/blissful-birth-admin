728f693d2334f0216ca151f580b50f1444ebf9e8
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2025-08-25 13:12:07 +0200

    refactor: added a loader for the delete modal

--- components/user/settings/appointments/[appointmentId]/users-delete-dialog.tsx
+++ components/user/settings/appointments/[appointmentId]/users-delete-dialog.tsx
@@ -1,8 +1,19 @@
 "use client";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { useState } from "react";
+import { LoaderCircle } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useUsers } from "./context/users-context";
 import { IconAlertTriangle } from "@tabler/icons-react";
-import { toast } from "@/hooks/use-toast";
-import { ConfirmDialog } from "@/components/confirm-dialog";
 import { deleteAppointment } from "@/actions/appointment";
+import { toast } from "@/hooks/use-toast";
 
 interface Props {
   userId: string;
@@ -18,7 +29,9 @@ export function DeleteAppointmentDialog({
   onOpenChange,
 }: Props) {
   const pathname = "/user/settings/" + userId + "/appointments";
-
+  const { setOpen } = useUsers();
+  const [isPending, setIspending] = useState(false);
+  const route = useRouter();
   const handleDelete = async () => {
     await deleteAppointment(appointmentId, pathname);
     onOpenChange(false);
@@ -35,31 +48,60 @@ export function DeleteAppointmentDialog({
   };
 
   return (
-    <ConfirmDialog
+    <Dialog
       open={open}
-      onOpenChange={onOpenChange}
-      handleConfirm={handleDelete}
-      title={
-        <span className="text-destructive">
-          <IconAlertTriangle
-            className="mr-1 inline-block stroke-destructive"
-            size={18}
-          />{" "}
-          Delete Appointment
-        </span>
-      }
-      desc={
-        <div className="space-y-4">
-          <p className="mb-2">
-            Are you sure you want to delete appointment entry?
-            <br />
-            This action will permanently remove the appointment entry from the
-            system. This cannot be undone.
-          </p>
+      onOpenChange={(state) => {
+        onOpenChange(state);
+      }}
+    >
+      <DialogContent className="sm:max-w-lg">
+        <DialogHeader className="text-left">
+          <DialogTitle className="text-red-600 flex flex-row items-center gap-2">
+            <IconAlertTriangle /> Delete Appointment
+          </DialogTitle>
+        </DialogHeader>
+        <ScrollArea className="-mr-4 w-full py-1 pr-4">
+          <div className="space-y-4">
+            <p className="mb-2 font-thin text-sm text-gray-900">
+              Are you sure you want to <b>delete</b> appointment entry?
+              <br />
+              This action will permanently remove the appointment entry from the
+              system. This cannot be undone.
+            </p>
+          </div>
+        </ScrollArea>
+        <div className="flex gap-2 justify-center">
+          <Button
+            type="button"
+            onClick={() => setOpen("")}
+            className="w-[120px] border text-black hover:bg-gray-200 bg-white-500"
+          >
+            Close
+          </Button>
+
+          {isPending ? (
+            <Button
+              type="button"
+              className="w-[120px] hover:bg-red-500 bg-red-500"
+            >
+              <LoaderCircle className="animate-spin" />
+            </Button>
+          ) : (
+            <Button
+              onClick={async () => {
+                setIspending(true);
+                await deleteAppointment(appointmentId, pathname);
+                route.push(pathname);
+              }}
+              className="bg-red-500 text-white hover:bg-red-600"
+              type="button"
+              form="user-form"
+            >
+              Delete
+            </Button>
+          )}
         </div>
-      }
-      confirmText="Delete"
-      destructive
-    />
+      </DialogContent>
+    </Dialog>
   );
 }
