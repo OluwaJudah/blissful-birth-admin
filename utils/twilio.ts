d89791d46ace3b17bc0d36f01c4429e596429538
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2025-08-31 22:47:30 +0200

    refactor: updated contentVariables for twilio

--- utils/twilio.ts
+++ utils/twilio.ts
@@ -10,11 +10,13 @@ export const sendWhatsApp = async (
   contentSid: string,
   contentVariables = ""
 ) => {
-  return await client.messages.create({
+  const msg: any = {
     contentSid,
-    contentVariables,
     from: `whatsapp:${process.env.TWILIO_WHATSAPP_NUMBER}`,
     to: `whatsapp:${to}`,
     statusCallback: `${process.env.BASE_URL}/api/twilio-status`,
-  });
+  };
+
+  if (contentVariables !== "") msg.contentVariables = contentVariables;
+  return await client.messages.create(msg);
 };
