5a0204d9e477b417b13e23ed0c48439e90983f64
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2025-09-30 14:48:11 +0200

    refactor: code cleanup

--- data/mother-info.ts
+++ data/mother-info.ts
@@ -256,10 +256,6 @@ export const getMotherInfoWithPaymentSum = async (
 ) => {
   await dbConnect();
 
-  await MotherInfo.updateMany(
-    { edd: { $ne: null }, status: "pending" }, // condition: date is not null
-    { $set: { status: "onboarded" } } // update: set status
-  );
   // Build matchStage dynamically
   const matchStage: any = {};
 
